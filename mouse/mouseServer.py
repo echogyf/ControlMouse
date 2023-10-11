ff0aa00718c2573868ac03dcf960286dd92f13de
郭宇峰 <echoodfeng@foxmail.com>
2023-10-11 11:41:52 +0800

    连接成功时优先获取屏幕分辨率

--- mouse/mouseServer.py
+++ mouse/mouseServer.py
@@ -1,6 +1,7 @@
 import socket
 import msgpack
 import ctypes
+import threading
 
 from pynput import mouse
 
@@ -11,16 +12,22 @@ def create_udp_socket(ip, port):
 
 # 发送主控端显示分辨率
 def getScreen():
-    # 获取当前屏幕分辨率
-    user32 = ctypes.windll.user32
-    width, height = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-    event = {
-        'type': 'Screen',
-        'width': width,
-        'height': height,
-    }
-    event_msgpack = msgpack.packb(event, use_bin_type=True)
-    udp_socket.sendto(event_msgpack, (remote_ip, remote_port))
+    try:
+        # 获取当前屏幕分辨率
+        user32 = ctypes.windll.user32
+        width, height = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+        event = {
+            'type': 'Screen',
+            'width': width,
+            'height': height,
+        }
+        event_msgpack = msgpack.packb(event, use_bin_type=True)
+        udp_socket.sendto(event_msgpack, (remote_ip, remote_port))
+        event_get_screen.set()  # 设置event_get_screen，表示已经获取屏幕分辨率
+    except Exception as e:
+        print(f"发生异常：{e}")
+        # 在发生异常时也要确保event_get_screen被设置，以便程序能继续执行
+        event_get_screen.set()
 
 # 发送鼠标移动事件
 def on_move(x, y):
@@ -70,25 +77,32 @@ def on_scroll(x, y, dx, dy):
     udp_socket.sendto(event_msgpack, (remote_ip, remote_port))
 
 def capture_and_send_click_mouse_events(udp_socket, remote_ip, remote_port):
+    event_get_screen.wait()  # 等待event_get_screen，确保getScreen已经执行
     with mouse.Listener(on_click=on_click, on_move=on_move, on_scroll=on_scroll) as listener:
         listener.join()
 
 # 主函数
 if __name__ == "__main__":
-    # remote_ip = '192.168.56.134' # 虚拟机
+    remote_ip = '192.168.56.134' # 虚拟机
     # remote_ip = '172.31.110.236' # 本机
-    remote_ip = '172.31.110.64' # 笔记本
+    # remote_ip = '172.31.110.64' # 笔记本
     remote_port = 12345  # 与被控端相同的端口
+
+    event_get_screen = threading.Event()
+    event_capture_mouse = threading.Event()
 
     # 创建UDP套接字
     udp_socket = create_udp_socket(remote_ip, remote_port)
-    getScreen() # 发送分辨率
+
     try:
         # 使用多线程或异步编程来运行鼠标事件捕获函数，以使操作不间断
         import threading
 
+        get_screen_thread = threading.Thread(target=getScreen)
+        get_screen_thread.start()
         mouse_click_thread = threading.Thread(target=capture_and_send_click_mouse_events, args=(udp_socket, remote_ip, remote_port))
         mouse_click_thread.start()
+        get_screen_thread.join()
         mouse_click_thread.join()
     except KeyboardInterrupt:
         print("连接已断开。")
